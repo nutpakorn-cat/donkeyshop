c7bd77d13c7404cd5c62a513c271ecb357cadad7
Nutpakorn Tassanasorn <twiceworldSoftware@gmail.com>
2022-11-16 00:31:20 +0700

    fix: use .eslintrc.js instead of .eslintrc.json

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: "next/core-web-vitals",
+  plugins: ["prettier", "unused-imports"],
+  rules: {
+    "prettier/prettier": "error",
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
