056e88567dc2797037f461346080db024093f968
Nutpakorn Tassanasorn <twiceworldSoftware@gmail.com>
2022-11-15 23:23:10 +0700

    fix: exclude test tsx file to prevent cypress jest conflict

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -1,10 +1,19 @@
-import Home from "../pages/index";
+import "@testing-library/jest-dom/extend-expect";
 import "@testing-library/jest-dom";
+
+import React from "react";
+import Home from "../pages/index";
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
 
 describe("Home", () => {
   it("must render correctly", () => {
-    render(<Home />);
+    render(
+      <Provider store={store}>
+        <Home />
+      </Provider>
+    );
     expect(screen.getByTestId("home.helloWorldText")).toBeInTheDocument();
   });
 });
